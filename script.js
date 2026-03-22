dd38251c1056a99e9aa7ef2a92b7c2d74d2ca29f
Existencio <arthemios@my.com>
2026-03-22 18:48:17 +0300

    Add basic task creation with JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const taskForm = document.querySelector('.task-form');
-const taskInput = document.getElementById('task-text');
-const taskList = document.getElementById('task-items');
-taskForm.addEventListener('submit', function(event) {
+const taskForm = document.querySelector('.task-form'); // находим форму по CSS селектору
+const taskInput = document.getElementById('task-text'); // находим поле ввода по id
+const taskList = document.getElementById('task-items'); // находим список задач по id
+
+taskForm.addEventListener('submit', function(event) {  // слушаем сабмит (отправку формы)
     event.preventDefault(); // отменяет стандартное действие браузера (не выполнять обычную отправку с перезагрузкой страницы)
-    console.log('форма отправлена')
+    let inputValue = taskInput.value.trim(); // находим значение от строки ввода (то, что ввёл пользователь)
+    if (inputValue === '') {
+        return
+    }
+    console.log(inputValue); // выводим значение из поля ввода в консоль
+
+    let taskUnit = document.createElement('li');
+    taskUnit.textContent = inputValue; // textContent помещает строку из поля ввода (inputValue) в DOM-элемент <li> (taskUnit)
+    taskList.appendChild(taskUnit); // добавляем узел - элемент <li> (taskUnit) в DOM - в в конец списка дочерних элементов указанного родительского узла (taskList)
+    taskInput.value = ''; // поле ввода очищается
+    taskInput.focus(); // после добавления задачи курсор снова ставится в поле ввода
 })
